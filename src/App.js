2fa02327fa821c5ed8b56d9975a50a8b5e82bade
tejaanuchuri0009 <tejaanuchuri0009@gmail.com>
2020-07-21 09:20:14 +0530

    "Hello_world...!"

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p> Hello_world...! React-Js-App</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
